1c45d67f3a7e4b468f3169dd58f3b1df57155409
hsrmy <hsrmy@emradc.xyz>
2021-04-23 22:50:38 +0900

    killされた時の次回起動時にpidファイルを消すように

--- kea-dhcp4/entrypoint.sh
+++ kea-dhcp4/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+LOCK_FILE="/usr/local/var/run/kea/kea-dhcp4.kea-dhcp4.pid"
+
+if [ -f ${LOCK_FILE} ]; then
+    rm -rf ${LOCK_FILE}
+fi
+
 exec /usr/local/sbin/kea-dhcp4 -c /etc/kea/kea-dhcp4.conf
--- kea-dhcp6/entrypoint.sh
+++ kea-dhcp6/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+LOCK_FILE="/usr/local/var/run/kea/kea-dhcp6.kea-dhcp6.pid"
+
+if [ -f ${LOCK_FILE} ]; then
+    rm -rf ${LOCK_FILE}
+fi
+
 exec /usr/local/sbin/kea-dhcp6 -c /etc/kea/kea-dhcp6.conf

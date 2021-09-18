083833db2e1bd475ac14f3d1f96c587f6046273d
hsrmy <hsrmy@emradc.xyz>
2021-09-19 01:35:54 +0900

    Digest認証部分をtreafikに移す

--- webdav/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-REALM="${REALM:-WebDAV}"
-
-if [ ! -e "/user.passwd" ]; then
-    HASH=$(printf '%s' "$USERNAME:$REALM:$PASSWORD"|md5sum|awk '{print $1}')
-    echo "$USERNAME:$REALM:$HASH" > /user.passwd
-fi
-
-exec "$@"
